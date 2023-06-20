998fb4fbedd9f38ca80dcbe08efd0da92e1d854a
artemplv <artemplv@gmail.com>
2023-06-20 15:04:47 -0400

    complete uniq

--- intro-to-js-exercises/phase-1-arrays.js
+++ intro-to-js-exercises/phase-1-arrays.js
@@ -1,1 +1,10 @@
 // Your code here
+Array.prototype.uniq = function() {  
+  const hash = {};
+  
+  this.forEach((el) => {
+    hash[el] = true;
+  });
+
+  return Object.keys(hash).map((el) => Number(el));
+}
