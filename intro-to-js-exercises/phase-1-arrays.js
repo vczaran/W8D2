c04de29a785a1d6eb2191407f01783c097888317
Viktoria Czaran <vczaran@gmail.com>
2023-06-20 15:49:57 -0400

    Completed prototype.twoSum

--- intro-to-js-exercises/phase-1-arrays.js
+++ intro-to-js-exercises/phase-1-arrays.js
@@ -15,3 +15,26 @@ Array.prototype.uniq = function() {
 
   // return Object.keys(hash).map((el) => Number(el));
 }
+
+
+Array.prototype.twoSum = function() {
+  const arr = this.toSorted((a, b) => a-b);
+  const pairs = [];
+  
+  let i = 0;
+  let j = (arr.length - 1);
+
+  while (i !== j) {
+    console.log(i);
+    console.log(j);
+    if ((arr[i] + arr[j]) === 0) {
+      pairs.push([this.indexOf(arr[i]), this.indexOf(arr[j])]);
+      j--;
+    } else if ((arr[i] + arr[j]) > 0) {
+      j--;
+    } else {
+      i++;
+    }
+  };
+  return pairs;
+}
